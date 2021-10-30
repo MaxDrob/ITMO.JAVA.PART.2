2cdd57ee88d0cb74af34693ccfedc6ac855e41d1
MaxDrob <83185232+MaxDrob@users.noreply.github.com>
2021-10-30 19:36:20 +0300

    Amended

--- src/com/maxdrob/company/Lesson4/AllTasksFromLesson4/L4T4.java
+++ src/com/maxdrob/company/Lesson4/AllTasksFromLesson4/L4T4.java
@@ -1,5 +1,17 @@
 package com.maxdrob.company.Lesson4.AllTasksFromLesson4;
 
 public class L4T4 {
+    public static void main(String[] args) {
 
+        String a = "TestTestThisIsATest";
+        String b = "Test";
+        int times = 0;
+        for (int i = 0; i <= a.length() - b.length(); i++) {
+            String substring = a.substring(i, i + b.length());
+            if (substring.equals(b)) {
+                times = times + 1;
+            }
+        }
+        System.out.println(times);
+    }
 }
