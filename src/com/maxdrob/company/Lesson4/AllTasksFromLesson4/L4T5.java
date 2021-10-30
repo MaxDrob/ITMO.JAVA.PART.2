a61afa362462e7e528634c626f0682f4939b87f7
MaxDrob <83185232+MaxDrob@users.noreply.github.com>
2021-10-30 20:14:53 +0300

    Amended

---
+++ src/com/maxdrob/company/Lesson4/AllTasksFromLesson4/L4T5.java
@@ -0,0 +1,9 @@
+package com.maxdrob.company.Lesson4.AllTasksFromLesson4;
+
+public class L4T5 {
+    public static void main(String args[]) {
+        StringBuffer buffer = new StringBuffer("Любая фраза");
+        buffer.reverse();
+        System.out.println(buffer);
+    }
+}
--- src/com/maxdrob/company/Lesson4/Lesson4Main.java
+++ src/com/maxdrob/company/Lesson4/Lesson4Main.java
@@ -1,4 +1,6 @@
 package com.maxdrob.company.Lesson4;
 
 public class Lesson4Main {
+
+    // nothing in here, please ignore
 }
