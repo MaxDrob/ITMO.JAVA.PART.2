0d40d32137fea268e9b8f5f23e9e3e06e9e77980
MaxDrob <83185232+MaxDrob@users.noreply.github.com>
2021-11-09 18:23:39 +0300

    Amended

---
+++ src/com/maxdrob/company/Lesson11/FileData.java
@@ -0,0 +1,46 @@
+package com.maxdrob.company.Lesson11;
+
+import java.io.BufferedReader;
+import java.io.FileReader;
+import java.io.IOException;
+
+public class FileData {
+    private String pathToFile;
+
+    public FileData(String path) {
+        pathToFile = path;
+    }
+
+    public String[] OpenAndRead() throws IOException {
+        FileReader x = new FileReader(pathToFile);
+        BufferedReader varRead = new BufferedReader(x);
+
+        int num = numStrings();
+        String[] lines = new String[num];
+
+        int i;
+        for (i = 0; i < num; i++) {
+            lines[i] = varRead.readLine();
+
+        }
+        varRead.close();
+        return lines;
+
+    }
+
+    int numStrings() throws IOException {
+
+        FileReader text = new FileReader(pathToFile);
+        BufferedReader y = new BufferedReader(text);
+
+        String one;
+        int num = 0;
+
+        while ((one = y.readLine()) != null) {
+            num++;
+        }
+        y.close();
+
+        return num;
+    }
+}
---
+++ src/com/maxdrob/company/Lesson11/Main.java
@@ -0,0 +1,24 @@
+package com.maxdrob.company.Lesson11;
+
+import java.io.IOException;
+
+
+public class Main {
+
+    public static void main(String[] args) throws IOException {
+        String new_path = "C:\\Users\\max33\\Desktop\\JavaTestText.txt";
+        try {
+            FileData new_file = new FileData(new_path);
+            String[] aryStrings = new_file.OpenAndRead();
+
+            int i;
+            for (i = 0; i < aryStrings.length; i++) {
+                System.out.println(aryStrings[i]);
+            }
+        }
+        catch (IOException a) {
+            System.out.println(a.getMessage());
+        }
+    }
+
+}
