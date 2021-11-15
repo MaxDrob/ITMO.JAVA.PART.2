0cd6a460b6091ebf8ae8f826fe042462c10e10ff
MaxDrob <83185232+MaxDrob@users.noreply.github.com>
2021-11-15 13:09:50 +0300

    Amended

---
+++ src/com/maxdrob/company/Lesson11Task4/Main.java
@@ -0,0 +1,24 @@
+package com.maxdrob.company.Lesson11Task4;
+
+import java.io.FileInputStream;
+import java.io.FileOutputStream;
+import java.io.IOException;
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+
+        FileInputStream fileIn =
+                new FileInputStream(
+                        "C:\\Users\\max33\\Desktop\\pngITMO.png");
+        FileOutputStream fileOut =
+                new FileOutputStream(
+                        "C:\\Users\\max33\\Desktop\\pngITMO2.png");
+
+        while (fileIn.available() > 0) {
+            int oneByte = fileIn.read();
+            fileOut.write(oneByte);
+        }
+        fileIn.close();
+        fileOut.close();
+    }
+}
