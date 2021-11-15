874f4e91999638b71d1c41c4ab94e0b4f6dbef58
MaxDrob <83185232+MaxDrob@users.noreply.github.com>
2021-11-15 12:46:32 +0300

    Amended

---
+++ src/com/maxdrob/company/Lesson11Task3/Main.java
@@ -0,0 +1,42 @@
+package com.maxdrob.company.Lesson11Task3;
+
+import java.io.BufferedReader;
+import java.io.FileReader;
+import java.io.IOException;
+import java.io.PrintWriter;
+
+public class Main
+{
+    public static void main(String[] args) throws IOException
+    {
+
+        PrintWriter pw = new PrintWriter("C:\\Users\\max33\\Desktop\\text3.txt");
+        BufferedReader br = new BufferedReader(new FileReader("C:\\Users\\max33\\Desktop\\text1.txt"));
+
+        String line = br.readLine();
+
+
+        while (line != null)
+        {
+            pw.println(line);
+            line = br.readLine();
+        }
+
+        br = new BufferedReader(new FileReader("C:\\Users\\max33\\Desktop\\text2.txt"));
+
+        line = br.readLine();
+
+        while(line != null)
+        {
+            pw.println(line);
+            line = br.readLine();
+        }
+
+        pw.flush();
+
+        br.close();
+        pw.close();
+
+        System.out.println("Файлы 1 и 2 записаны в файл 3");
+    }
+}
